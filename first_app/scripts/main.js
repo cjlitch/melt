35e4b0414ec603e90e406430cab335027d3c3c87
Caroline Litchfield <cjlitch@gmail.com>
2015-02-01 23:49:35 -0500

    footer and icons

--- first_app/scripts/main.js
+++ first_app/scripts/main.js
@@ -1,30 +1,49 @@
 if (Meteor.isClient) {
 
-    Template.about.events({
-        'click': function() {
-            var _this = event.target,
-                scrollSelector = $(_this).data('scroll'),
-                scrollToDiv = $(scrollSelector);
+    var navClickHandler = function(event) {
+        var _this = event.target,
+            scrollSelector = $(_this).data('scroll'),
+            scrollToDiv = $(scrollSelector);
 
-            $('html, body')
-                .animate({
-                    scrollTop: scrollToDiv.offset().top - 60
-                }, 300);
-        }
+        $('html, body')
+            .animate({
+                scrollTop: scrollToDiv.offset().top - 60
+            }, 300);
+    };
+
+    var postRenderScroll = function() {
+        $(document).ready(function() {
+            $('html, body').scrollTop(0);
+        });
+    };
+
+    Template.main.postRender = function () {
+        postRenderScroll();
+    };
+
+    Template.about.events({
+        'click': navClickHandler
     });
+
+    Template.about.postRender = function () {
+        postRenderScroll();
+    };
 
     Template.massage.events({
-        'click': function() {
-            var _this = event.target,
-                scrollSelector = $(_this).data('scroll'),
-                scrollToDiv = $(scrollSelector);
-
-            $('html, body')
-                .animate({
-                    scrollTop: scrollToDiv.offset().top - 60
-                }, 300);
-        }
+        'click': navClickHandler
     });
+
+    Template.massage.postRender = function () {
+        postRenderScroll();
+    };
+
+    Template.classes.events({
+        'click': navClickHandler
+    });
+
+    Template.classes.postRender = function () {
+        postRenderScroll();
+    };
 }
 
 
